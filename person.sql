a0a60695ec93c726b35842ed12193e7746e25e82
오소현 <53892427+osohyun0224@users.noreply.github.com>
2023-04-13 13:50:11 +0900

    Add files via upload

---
+++ person.sql
@@ -0,0 +1,2 @@
+INSERT INTO person (person_name, person_address, job) VALUES ('오소현', '서울', {'정보컴퓨터교사', '프론트엔드 개발자'});
+INSERT INTO person (person_name, person_address, job) VALUES ('오민혁', '서울', {'모빌리티 개발자', '한양대학교 졸업생'});
